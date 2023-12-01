6ae4bd0ac0ba9b4ab9d69c68a2c7a71b10b29f6a
Yuri Cruger <aoddrewiid@gmail.com>
2023-12-01 18:37:31 -0300

    first commit

---
+++ src/components/footer-components/InputFormError.tsx
@@ -0,0 +1,11 @@
+
+interface ErrorMsgProps {
+    message: string;
+  }
+export default function InputFormError({ message }:ErrorMsgProps) {
+  return (
+    <div className="text-red-500 text-sm mt-1">
+      {message}
+    </div>
+  )
+}
